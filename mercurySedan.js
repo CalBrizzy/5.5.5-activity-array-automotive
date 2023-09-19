ada204303180cd4416701e678119e60c5e66c883
cbrizzy003@gmail.com <cbrizzy003@gmail.com>
2023-09-19 01:10:22 -0500

    made some new code

--- mercurySedan.js
+++ mercurySedan.js
@@ -1,9 +1,8 @@
 //this includes the vehicle class as a module
-const VehicleModule = require("./vehicleBaseClass")
+const VehicleModule = require("./vehicleBaseClass").Vehicle
 
 //this shows how to call from this module...
-let v = new VehicleModule.Vehicle("Mercury", "Sedan", "1965", "color", "mileage");
-console.log(v.make)
+
 
 
 //After you write the derived Car class, you should test it out.
@@ -12,15 +11,71 @@ console.log(v.make)
 
 
 //TO DO: Code the Car subclass here or in index.js file, i.e. class Car extends Vehicle ...
+class Car extends VehicleModule {
+    constructor (make, model, year, color, mileage) {
+        super (make, model, year, color, mileage)
+        this.maximumPassengers = 5
+        this.passenger = 0
+        this.numberOfWheels = 4
+        this.maximumSpeed = 160
+        this.fuel = 10
+        this.scheduleService = false
+    }
 
+    loadPassenger (num) {
+        if (num > this.maximumPassengers) {
+            this.passenger = num
+            console.log(`This ${this.make} ${this.model} cannot seat ${this.passenger} people`)
+        }
+        else if (num === this.maximumPassengers) {
+            this.passenger = num
+            console.log (`This ${this.make} ${this.model} is at full capacity`)
+        }
+        else
+        {
+            this.passenger = num
+            console.log (`This ${this.make} ${this.model} can still seat ${(this.maximumPassengers - this.passenger)} people`)
+        }
+    }
 
+    start () {
+        if (this.fuel > 0) {
+            this.started = true
+            console.log (`This ${this.make} ${this.model} has enough fuel to start`)
+        }
+        else 
+        {
+            console.log (`This ${this.make} ${this.model} has no fuel`)
+        }
+    }
 
+    service (mileage) {
+        if ((mileage + this.mileage) >= 30000) {
+            this.scheduleService = true
+            console.log (`This ${this.make} ${this.model} needs a service`)
+        }
+        else
+        {
+            console.log (`This ${this.make} ${this.model} doesn't need a service`)
+        }
+    }
+}
 
+let vehicle1 = new Car ('Mercury', 'A28', '1995', 'Black', 45000)
 
+vehicle1.loadPassenger(4)
+vehicle1.start()
+vehicle1.service(vehicle1.mileage)
 
+console.log (vehicle1)
 
+let vehicle2 = new Car ('Ford', 'F-150', 2020, 'Red', 5000)
 
+vehicle2.loadPassenger(2)
+vehicle2.start()
+vehicle2.service(vehicle2.mileage)
 
+console.log (vehicle2)
 
 
 
